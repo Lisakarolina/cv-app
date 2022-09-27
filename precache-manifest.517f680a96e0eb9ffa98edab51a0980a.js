addae7d0ece235a0c335365f8801e9d2d69a4971
Lisakarolina <w.hello@web.de>
2022-09-27 19:53:23 +0200

    Updates

---
+++ precache-manifest.517f680a96e0eb9ffa98edab51a0980a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ccd71817d64dac3a465",
+    "url": "/cv-app/static/css/main.25a49003.chunk.css"
+  },
+  {
+    "revision": "5ccd71817d64dac3a465",
+    "url": "/cv-app/static/js/main.5ccd7181.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/cv-app/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "31f8b5e24d67d359c356",
+    "url": "/cv-app/static/js/2.31f8b5e2.chunk.js"
+  },
+  {
+    "revision": "2b63ba983ed9d8d01435",
+    "url": "/cv-app/static/js/runtime~main.2b63ba98.js"
+  },
+  {
+    "revision": "cc88fc35d486ff015399428f6ef0a3be",
+    "url": "/cv-app/index.html"
+  }
+];
