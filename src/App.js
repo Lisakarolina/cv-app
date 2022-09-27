e8c6405add119b1dfedd23edc6be8c946dae2b25
Lisakarolina <w.hello@web.de>
2022-09-27 16:09:15 +0200

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Field } from "./components/fields.js";
 import { MirrorField } from "./components/mirror-fields.js";
